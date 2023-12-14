c2eacf58544b7da61f10aae573b4aac523599ab2
alexkols <kolosoff.alex@gmail.com>
2023-12-14 21:30:48 +0200

    small refactor. edit sender

--- src/main/java/md/akdev/loyality_cms/service/DeviceService.java
+++ src/main/java/md/akdev/loyality_cms/service/DeviceService.java
@@ -1,31 +1,26 @@
 package md.akdev.loyality_cms.service;
 
-import md.akdev.loyality_cms.dto.ClientDeviceDto;
 import md.akdev.loyality_cms.model.DevicesModel;
 import md.akdev.loyality_cms.repository.DevicesRepository;
-import md.akdev.loyality_cms.utils.MappingUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DeviceService {
-    private final MappingUtils mappingUtils;
+//    private final MappingUtils mappingUtils;
     public final DevicesRepository devicesRepository;
 
-    public DeviceService(MappingUtils mappingUtils, DevicesRepository devicesRepository) {
-        this.mappingUtils = mappingUtils;
+    public DeviceService(DevicesRepository devicesRepository) {
+//        this.mappingUtils = mappingUtils;
         this.devicesRepository = devicesRepository;
     }
 
-    public DevicesModel mapToDeviceModel(ClientDeviceDto dto) {
-        return mappingUtils.mapToDeviceModel(dto);
-    }
+//    public DevicesModel mapToDeviceModel(ClientDeviceDto dto) {
+//        return mappingUtils.mapToDeviceModel(dto);
+//    }
 
     public ResponseEntity<?> addDevice(DevicesModel inputDevice) {
-        DevicesModel getDevice = devicesRepository.getDeviceByDeviceId(inputDevice.getDeviceId());
-        if (getDevice == null) {
             devicesRepository.save(inputDevice);
-        } devicesRepository.save(getDevice);
         return ResponseEntity.ok("successful");
     }
 
