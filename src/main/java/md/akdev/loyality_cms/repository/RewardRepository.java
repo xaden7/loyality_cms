e7b6d0560d375078c7f9b5902ba89c49a4b0098e
vbrizitschi <vbrizitschi@outlook.com>
2024-02-29 08:39:58 +0200

    modify RewardService

--- src/main/java/md/akdev/loyality_cms/repository/RewardRepository.java
+++ src/main/java/md/akdev/loyality_cms/repository/RewardRepository.java
@@ -9,14 +9,18 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public interface RewardRepository extends JpaRepository<Reward,Integer> {
     @Query("select p from Reward p where ?1 between p.dateFrom and p.dateTo and p.rewardType = ?2")
     List<Reward> findActiveRewards(LocalDate now, RewardsType rewardType);
 
+    @Query("select p from Reward p where ?1 between p.dateFrom and p.dateTo and p.id not in (select r.reward.id from RewardUsed r where r.client.id = ?2)")
+    List<Reward> findAllActiveRewards(LocalDate now, UUID clientId);
+
     @Query("select p from Reward p where ?1 between p.dateFrom and p.dateTo")
-    List<Reward> findAllActiveRewards(LocalDate now);
+    List<Reward> findAllActiveRewardsNotAuth(LocalDate now);
 
     @Query( value = "select * from rewards where ( :dateFrom between date_from and date_to or :dateTo between date_from and date_to ) and reward_type = :rewardType", nativeQuery = true)
     List<Reward> findSomeTypeRewards(@Param("dateFrom") LocalDate dateFrom, @Param("dateTo") LocalDate dateTo, @Param("rewardType") Integer rewardType);
--- src/main/java/md/akdev/loyality_cms/service/RewardService.java
+++ src/main/java/md/akdev/loyality_cms/service/RewardService.java
@@ -1,35 +1,55 @@
 package md.akdev.loyality_cms.service;
 
+import md.akdev.loyality_cms.model.ClientsModel;
+import md.akdev.loyality_cms.model.JwtAuthentication;
 import md.akdev.loyality_cms.model.Reward;
+import md.akdev.loyality_cms.repository.ClientsRepository;
 import md.akdev.loyality_cms.repository.RewardRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class RewardService {
 
     private final RewardRepository rewardRepository;
+    private final JwtAuthService jwtAuthService;
 
+    private final ClientsRepository clientsRepository;
     @Autowired
-    public RewardService(RewardRepository rewardRepository) {
+    public RewardService(RewardRepository rewardRepository, JwtAuthService jwtAuthService, ClientsRepository clientsRepository) {
         this.rewardRepository = rewardRepository;
+        this.jwtAuthService = jwtAuthService;
+        this.clientsRepository = clientsRepository;
     }
-
 
     public Optional<Reward> findById(Integer id){
         return rewardRepository.findById(id);
     }
 
     public List<Reward> findAll(){
-//        return rewardRepository.findAll();
-        return rewardRepository.findAllActiveRewards(LocalDate.now());
+
+        if (SecurityContextHolder.getContext().getAuthentication().getPrincipal() == "anonymousUser") {
+            return rewardRepository.findAllActiveRewardsNotAuth(LocalDate.now());
+        }
+
+        final JwtAuthentication authentication = jwtAuthService.getAuthInfo();
+        ClientsModel clientsModel = clientsRepository.getClientByUuid1c(authentication.getUuid());
+
+        if (clientsModel.getId() != null) {
+            rewardRepository.findAllActiveRewards(LocalDate.now(), clientsModel.getId());
+        }
+
+        return new ArrayList<>();
     }
 
 }
