f7d9f16ddec6b7153b4d47a4165ece30a406f121
Colosov Alexei <kolosoff.alex@gmail.com>
2024-11-20 21:33:25 +0200

    add partner image method v1

--- src/main/java/md/akdev/loyality_cms/restController/PartnerController.java
+++ src/main/java/md/akdev/loyality_cms/restController/PartnerController.java
@@ -2,9 +2,15 @@ package md.akdev.loyality_cms.restController;
 
 import lombok.RequiredArgsConstructor;
 import md.akdev.loyality_cms.model.partners.Partner;
+import md.akdev.loyality_cms.model.partners.PartnerImage;
 import md.akdev.loyality_cms.service.PartnerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin
@@ -20,6 +26,25 @@ public class PartnerController {
     public ResponseEntity<Iterable<Partner>> getAllPartners() {
         Iterable<Partner> partners = partnerService.getAllPartners();
         return ResponseEntity.ok(partners);
+    }
+
+    @GetMapping("/get-image")
+    public ResponseEntity<?> getImage(UUID id) {
+
+        Optional<PartnerImage> partnerImage = partnerService.findImageById(id);
+
+        String data = partnerImage.isPresent() ? partnerImage.get().getBody() : "";
+
+        if (data.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.valueOf("image/jpeg")).body(new byte[0]);
+        }
+
+        String base64Image = data.split(",")[1];
+        byte[] imageBytes = java.util.Base64.getDecoder().decode(base64Image);
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.IMAGE_JPEG)
+                .body(imageBytes);
+
     }
 
 }
--- src/main/java/md/akdev/loyality_cms/service/PartnerService.java
+++ src/main/java/md/akdev/loyality_cms/service/PartnerService.java
@@ -3,22 +3,29 @@ package md.akdev.loyality_cms.service;
 import lombok.RequiredArgsConstructor;
 
 import md.akdev.loyality_cms.model.partners.Partner;
+import md.akdev.loyality_cms.model.partners.PartnerImage;
+import md.akdev.loyality_cms.repository.PartnerImageRepository;
 import md.akdev.loyality_cms.repository.PartnerRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class PartnerService {
     private final PartnerRepository partnerRepository;
+    private final PartnerImageRepository partnerImageRepository;
 
     public List<Partner> getAllPartners() {
         return partnerRepository.findAll();
     }
 
 
-
+    public Optional<PartnerImage> findImageById(UUID id) {
+        return partnerImageRepository.findById(id);
+    }
 }
 
 
