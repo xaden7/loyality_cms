4c49bf429ffdc166b5883d83b90e933c6443a299
vbrizitschi <vbrizitschi@outlook.com>
2025-04-30 15:02:43 +0300

    Add Notification CRUD

--- src/main/java/md/akdev/loyality_cms/config/ObjectMapperConfig.java
+++ src/main/java/md/akdev/loyality_cms/config/ObjectMapperConfig.java
@@ -1,36 +1,36 @@
-package md.akdev.loyality_cms.config;
-
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.*;
-import com.fasterxml.jackson.databind.introspect.VisibilityChecker;
-import com.fasterxml.jackson.databind.ser.DefaultSerializerProvider;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.io.IOException;
-@Configuration
-public class ObjectMapperConfig {
-    @Bean
-    public ObjectMapper objectMapper(){
-
-        DefaultSerializerProvider sp = new DefaultSerializerProvider.Impl();
-        sp.setNullValueSerializer(new JsonSerializer<Object>() {
-            public void serialize(Object value, JsonGenerator jgen,
-                                  SerializerProvider provider)
-                    throws IOException
-            {
-                jgen.writeString("");
-            }
-        });
-
-        return new ObjectMapper()
-                .setVisibility(VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY))
-                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-                .findAndRegisterModules()
-                .setSerializerProvider(sp
-                )
-                ;
-    }
-}
+//package md.akdev.loyality_cms.config;
+//
+//import com.fasterxml.jackson.annotation.JsonAutoDetect;
+//import com.fasterxml.jackson.core.JsonGenerator;
+//import com.fasterxml.jackson.databind.*;
+//import com.fasterxml.jackson.databind.introspect.VisibilityChecker;
+//import com.fasterxml.jackson.databind.ser.DefaultSerializerProvider;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.io.IOException;
+//@Configuration
+//public class ObjectMapperConfig {
+//    @Bean
+//    public ObjectMapper objectMapper(){
+//
+//        DefaultSerializerProvider sp = new DefaultSerializerProvider.Impl();
+//        sp.setNullValueSerializer(new JsonSerializer<Object>() {
+//            public void serialize(Object value, JsonGenerator jgen,
+//                                  SerializerProvider provider)
+//                    throws IOException
+//            {
+//                jgen.writeString("");
+//            }
+//        });
+//
+//        return new ObjectMapper()
+//                .setVisibility(VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY))
+//                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+//                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+//                .findAndRegisterModules()
+//                .setSerializerProvider(sp
+//                )
+//                ;
+//    }
+//}
